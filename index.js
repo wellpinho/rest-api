55c8ba2962dd7212f480fede9bdebe14a17d17e8
wellpinho <wellpinho@outlook.com>
2020-12-06 01:44:38 -0300

    Tratamento de erros simples

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+
+const Product = require('./src/routes/ProductRoutes')
+const Requests = require('./src/routes/PedidosRoutes')
 
 app.get('/', (req, res, next) => {
   return res.status(200).send('Welcome')
+})
+
+app.use(morgan('dev'))
+app.use(Product)
+app.use(Requests)
+
+app.use((req, res, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  return res.send({
+    message: error.message
+  })
 })
 
 app.listen(PORT = 4000, () => {
